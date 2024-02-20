6c9973908022fe3a1989f54d3745694e1f364161
DharunRaju005 <dharunraju22@gmail.com>
2024-02-20 05:42:07 +0530

    Added QCodeGeneator

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ function App() {
 
       {/* <LoadMore/> */}
 
-      <TreeView menus={menus}/>
+      {/* <TreeView menus={menus}/> */}
 
-      {/* <QrGenerator/> */}
+      <QrGenerator/>
 
     </div>
   );
--- src/components/qr-generator/QrGenerator.jsx
+++ src/components/qr-generator/QrGenerator.jsx
@@ -1,26 +1,45 @@
 import styled from "styled-components";
-import QRCode from "react-qr-code"
+import QRCode from "react-qr-code";
+import { useState } from "react";
 
-const QrGenerator=()=>{
-    return(<>
-        <Container>
-            <h1>Qr Code Generator</h1>
-            <Input type="text" name="qr-code" placeholder="Entr the value"/> 
-        </Container>
-        <QrHolder>
-          <QRCode id="qr-code-value" value={qrCode} size={400} bgColor="#fff" />
-        </QrHolder>
+const QrGenerator = () => {
+  const [qrCode, setQrCode] = useState("");
+  const [input, setInput] = useState("");
 
-        </>
-
-
-    );
+  const handleGenerateQrCode = () => {
+    setQrCode(input);
+    setInput("");
+  };
+  return (
+    <>
+      <InputContainer>
+        <h1>Qr Code Generator</h1>
+        <Input onChange={(e) => setInput(e.target.value)} type="text" name="qr-code" placeholder="Entr the value" />
+        <button onClick={() => handleGenerateQrCode} disabled={input && input.trim() !== "" ? false : true}>
+          Generate
+        </button>
+      </InputContainer>
+      <QrHolder>
+        <QRCode id="qr-code-value" value={qrCode} size={400} bgColor="#fff" />
+      </QrHolder>
+    </>
+  );
 };
 
-const Container=styled.div``;
+const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 30px;
+`;
 
-const Input=styled.input``;
+const Input = styled.input``;
 
-const QrHolder=styled.div``;
+const QrHolder = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 3rem;
+`;
 
 export default QrGenerator;
